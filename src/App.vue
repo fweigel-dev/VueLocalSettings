4c914088229f110e3c8fb8a1a9b7468f0284dace
Ferox-Stylo <fabian.weigel1@web.de>
2024-01-22 12:37:38 +0100

    :lipstick: import dashboard into app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import SettingsContainer from "./components/SettingsContainer.vue";
+import Dashboard from "./components/Dashboard.vue";
 </script>
 
 <template>
+  <Dashboard />
   <SettingsContainer />
 </template>
 
