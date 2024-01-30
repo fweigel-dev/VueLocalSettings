e214e1e0d73d3e0e2beda61ddd1a38d583c19293
Ferox-Stylo <fabian.weigel1@web.de>
2024-01-30 14:55:06 +0100

    :lipstick: remove unnecessary gap

--- src/components/SettingsSelectNone.vue
+++ src/components/SettingsSelectNone.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="settings-item">
-    <div class="flex flex-col gap-2">
       <span class="settings-title">{{ config.title }}</span>
       <p class="settings-description">{{ config.description }}</p>
-    </div>
   </div>
 </template>
 
